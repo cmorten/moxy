b0d555155206298f48cbdaaf53cf5ef92550c547
asos-craigmorten <craig.morten@asos.com>
2020-06-30 01:38:48 +0100

    feat: spy moxy

--- README.md
+++ README.md
@@ -29,3 +29,47 @@ deno run --allow-net "https://raw.githubusercontent.com/asos-craigmorten/moxy/ma
 ```
 
 In this example there is a 50% chance that the `noop.ts` module is imported and a 50% chance that the `echoMeta.ts` module is imported.
+
+### Spy
+
+Wraps every function export of the provided module in a spy.
+
+```ts
+Deno.test(
+  "it should spy on every function export of the target module",
+  async () => {
+    /**
+     * We import the moxy spy module with our target module set to the `m`
+     * query-string parameter.
+     */
+    const nodule = await import(
+      `https://deno.land/x/gh:asos-craigmorten:moxy@main/spy.ts?m=${encodeURIComponent(
+        "https://deno.land/x/opine@main/mod.ts"
+      )}`
+    );
+
+    /**
+     * We can now get our target module exports through a slightly adapted
+     * async `.default()` API.
+     */
+    const { opine } = await nodule.default();
+
+    /**
+     * Our import works as it would normally.
+     */
+    const app = opine();
+
+    app.get("/", (req: any, res: any) => {
+      res.send("Hello Deno!");
+    });
+
+    await superdeno(app).get("/").expect("Hello Deno!");
+
+    /**
+     * But we can see here that we have actually been spying on it!
+     */
+    expect(opine.calls[0].args).toEqual([]);
+    expect(opine.calls[0].returned).toEqual(app);
+  }
+);
+```
--- spy.test.ts
+++ spy.test.ts
@@ -1,7 +1,40 @@
-Deno.test("it should spy on every function export of the target module", async () => {
-  const nodule = await import(
-    "https://raw.githubusercontent.com/asos-craigmorten/moxy/main/spyb.ts\?m\=https%3A%2F%2Fraw.githubusercontent.com%2Fasos-craigmorten%2Fopine%2Fmain%2Fmod.ts"
-  );
+import { expect } from "https://deno.land/x/expect@285caf/mod.ts";
+import { superdeno } from "https://deno.land/x/superdeno@1.5.0/mod.ts";
 
-  console.log("asd", nodule.default);
-});
+Deno.test(
+  "it should spy on every function export of the target module",
+  async () => {
+    /**
+     * We import the moxy spy module with our target module set to the `m`
+     * query-string parameter.
+     */
+    const nodule = await import(
+      `https://deno.land/x/gh:asos-craigmorten:moxy@main/spy.ts?m=${encodeURIComponent(
+        "https://deno.land/x/opine@main/mod.ts"
+      )}`
+    );
+
+    /**
+     * We can now get our target module exports through a slightly adapted
+     * async `.default()` API.
+     */
+    const { opine } = await nodule.default();
+
+    /**
+     * Our import works as it would normally.
+     */
+    const app = opine();
+
+    app.get("/", (req: any, res: any) => {
+      res.send("Hello Deno!");
+    });
+
+    await superdeno(app).get("/").expect("Hello Deno!");
+
+    /**
+     * But we can see here that we have actually been spying on it!
+     */
+    expect(opine.calls[0].args).toEqual([]);
+    expect(opine.calls[0].returned).toEqual(app);
+  }
+);
